cda0164f7caddeb639b4566407bfe3161c0a1354
codebreaker69 <33565436+codebreaker69@users.noreply.github.com>
2019-07-17 12:27:10 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "885236927:AAFezfTFuB3T1qOtu5m_TKzlCWIw-Noxp2M"
-    CLIENT_ID = "893791188621-0rgqkj4m0eau1nb6et2cfr6kkvkq8v29.apps.googleusercontent.com"
-    CLIENT_SECRET = "UMhd2miY8pJ75MouH2x2BqHZ"
-    PATH_ID = "1nGh_CeD2RYlhNzu5DHFyF2H6UmAE7eEt"
-    USER_ID = "899307621"
+    TOKEN = ""
+    CLIENT_ID = ""
+    CLIENT_SECRET = ""
+    PATH_ID = ""
+    USER_ID = ""
