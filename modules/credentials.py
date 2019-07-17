c0928be8ad7a727608a8a5ad18713984679796dc
codebreaker69 <33565436+codebreaker69@users.noreply.github.com>
2019-07-17 12:16:12 +0530

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "bot token here"
-    CLIENT_ID = "client id from google cloud console"
-    CLIENT_SECRET = "client secret from google cloud console"
-    PATH_ID = "folder id of the gdrive folder in which the bot will upload its files"
-    USER_ID = "your telegram chat id"
+    TOKEN = "885236927:AAFezfTFuB3T1qOtu5m_TKzlCWIw-Noxp2M"
+    CLIENT_ID = "893791188621-0rgqkj4m0eau1nb6et2cfr6kkvkq8v29.apps.googleusercontent.com"
+    CLIENT_SECRET = "UMhd2miY8pJ75MouH2x2BqHZ"
+    PATH_ID = "1nGh_CeD2RYlhNzu5DHFyF2H6UmAE7eEt"
+    USER_ID = "899307621"
